477b1a5468cfa799a2e5dbbdd8700901445ad845
akale9888 <akale9888@gmail.com>
2022-10-01 19:29:26 +0530

    basic s3 created

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIARP5PLQ5KLE4PG6JW"
+  secret_key = "a+GhJEGZWwehmM+VR9Mb4evUC0qwOTx2sAr/5jes"
+}
+resource "aws_s3_bucket" "s3bucket" {
+  bucket = "s3terraformbucket"
+}
+
+# resource "aws_s3_bucket_acl" "example" {
+#   bucket = aws_s3_bucket.examplebucket.id
+#   acl    = "private"
+# }
